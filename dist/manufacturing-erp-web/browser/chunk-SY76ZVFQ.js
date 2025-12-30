a74b209e0e1276996896523eea4e8002e72e5c19
mTahirP <tpatel@digirestrosols.in>
2025-12-30 10:51:03 +0530

    Remove Angular cache from Git tracking
    Remove Angular cache from Git tracking

--- dist/manufacturing-erp-web/browser/chunk-SY76ZVFQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-TMC7WMLO.js";var t=[{path:"",redirectTo:"orders",pathMatch:"full"},{path:"orders",loadComponent:()=>import("./chunk-Y274TROB.js").then(o=>o.SalesOrdersComponent)},{path:"tracking",loadComponent:()=>import("./chunk-U7XWRPZM.js").then(o=>o.SalesTrackingComponent)}];export{t as SALES_ROUTES};
